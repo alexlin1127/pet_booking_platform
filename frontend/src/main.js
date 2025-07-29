00e16d78d08f757337a1e15d164c09e04ba48e19
TingShen <tingshen0609@gmail.com>
2025-07-29 15:56:46 +0800

    feat: 新增 FontAwesome 圖示庫依賴

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faPlay, faAngleLeft, faAngleRight)
+
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 createApp(App).use(router).mount('#app')
