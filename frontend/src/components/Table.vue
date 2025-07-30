3ab2f1a93043eee9d6de526dea38c92ef7c921f9
TingShen <tingshen0609@gmail.com>
2025-07-30 15:37:59 +0800

    feat: 更新表格組件以使用 setup 語法

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,3 +1,18 @@
-<script></script>
-<template></template>
+<script setup></script>
+
+<template>
+        <table class="table">
+        <thead>
+            <tr class="table-header">
+                <slot name="header"></slot>
+            </tr>
+        </thead>
+        <tbody class="table-body">
+            <slot name="body"></slot>
+        </tbody>
+    </table>
+</template>
+
+
+
 <style></style>
