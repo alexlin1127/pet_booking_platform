0e7876ace9b9a35e6ef95477cb3a78ffeea70560
TingShen <tingshen0609@gmail.com>
2025-07-30 15:37:19 +0800

    feat: 新增多個樣式檔案以支援管理與使用者介面

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+// 引入css套件
+import './style.css'
+
+import './styles/main.css'
+
+import './styles/components/navbar.css'
+import './styles/components/footer.css'
+import './styles/components/form.css'
+import './styles/components/table.css'
+import './styles/components/sidebar.css'
+
+import './styles/button.css'
+
+import './styles/Admin/card.css'
+
+import './styles/Users/register.css'
 
 library.add(faPlay, faAngleLeft, faAngleRight)
 
