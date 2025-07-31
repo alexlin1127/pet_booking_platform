a38723ad3a64e9207759417d3643a36ffd2c2950
TingShen <tingshen0609@gmail.com>
2025-07-31 17:17:29 +0800

    fix: 修正登入頁面路由大小寫不一致問題

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -4,7 +4,7 @@
 
 <template>
     <p>這是登入畫面</p>
-    <RouterLink to="/Register/Users">使用者註冊</RouterLink>
-    <RouterLink to="/Register/Shops/1">商家註冊</RouterLink>
+    <RouterLink to="/register/users">使用者註冊</RouterLink>
+    <RouterLink to="/register/shops/1">商家註冊</RouterLink>
 </template>
 <style></style>
