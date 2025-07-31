882a0a81cf1f3a9d27f74217ecb03e2dd220d482
TingShen <tingshen0609@gmail.com>
2025-07-31 11:53:35 +0800

    feat: 新增貼文管理頁面及其功能

---
+++ frontend/src/pages/Admin/Card/PostCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+  post:Object
+});
+
+</script>
+
+<template>
+  <td>{{ post.id }}</td>
+  <td>{{ post.storeName }}</td>
+  <td>{{ post.publishDate }}</td>
+  <td>{{ post.title }}</td>
+  <td>{{ post.status }}</td>
+  <td v-if="post.status === '待審核'">
+    <RouterLink :to="`/Admin/Post/review?id=${post.id}&name=${post.postName}`">
+      <button class="btn">查看並審核</button>
+    </RouterLink>
+  </td>
+  <td v-else>
+    <RouterLink :to="`/Admin/Post/details?id=${post.id}&name=${post.postName}`">
+      <button class="btn">店家詳情</button>
+    </RouterLink>
+  </td>
+</template>
+
+<style></style>
