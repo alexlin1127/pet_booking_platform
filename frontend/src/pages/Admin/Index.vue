cef297151a2bf6606c2ef39b62633d0990900176
TingShen <tingshen0609@gmail.com>
2025-07-31 11:53:58 +0800

    feat: 新增管理頁面的結構與內容

--- frontend/src/pages/Admin/Index.vue
+++ frontend/src/pages/Admin/Index.vue
@@ -1,3 +1,13 @@
 <script></script>
-<template></template>
+<template>
+    <div>
+        <h1 class="storemanage-title">重要通知</h1>
+    </div>
+    <div>
+        <h1 class="storemanage-title">營運概況</h1>
+    </div>
+    <div>
+        <h1 class="storemanage-title">近期店家動態</h1>
+    </div>
+</template>
 <style></style>
