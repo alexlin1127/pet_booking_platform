dc8296494422051394ac60c615486c1537cc6899
TingShen <tingshen0609@gmail.com>
2025-08-01 10:26:15 +0800

    feat: 新增管理儀表板頁面及更新結構

---
+++ frontend/src/pages/Admin/AdminDashboard.vue
@@ -0,0 +1,30 @@
+<!-- <script setup>
+import { ref ,computed } from 'vue'
+import { useRoute , useRouter } from 'vue-router'
+import Card from '../../components/UI/Card.vue'
+
+const route = useRoute()
+const router = useRouter()
+
+const totalusers = ref(520)
+const totalshops = ref(12)
+</script>
+
+<template>
+    <h1 class="storemanage-title">重要通知</h1>
+    <div>
+        <Card>
+            
+        <Card/>
+    </div>
+    <hr/>
+    <p>目前用戶人數 totalusers / 店家 {totalshops} 間</p>
+    <hr/>
+    <div>
+        <h1 class="storemanage-title">營運概況</h1>
+    </div>
+</template>
+<style></style> -->
+<script></script>
+<template></template>
+<style></style>
