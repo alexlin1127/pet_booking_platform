7e590977820812e4cc205ee04bb7889fb385fee8
TingShen <tingshen0609@gmail.com>
2025-08-01 10:26:02 +0800

    feat: 新增登入、重設密碼及歡迎頁面

---
+++ frontend/src/auth/Login.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <p>這是登入畫面</p>
+    <RouterLink to="/register/users">使用者註冊</RouterLink>
+    <RouterLink to="/register/shops/1">商家註冊</RouterLink>
+</template>
+<style></style>
