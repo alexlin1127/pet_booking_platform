f0bd8646a579137f55c50b2def132a6394b049a9
TingShen <tingshen0609@gmail.com>
2025-08-05 15:09:55 +0800

    feat: 新增註冊頁面

---
+++ frontend/src/auth/Register.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import Card from '../components/UI/Card.vue';
+</script>
+
+<template>
+    <h1 class="text-4xl text-center font-bold">選擇註冊身分</h1>
+    <!-- 左<Card /> -->
+    <!-- 右<Card/> -->
+    <RouterLink to="/register/customers">使用者註冊</RouterLink>
+    <RouterLink to="/register/stores/1">商家註冊</RouterLink>
+</template>
+<style></style>
