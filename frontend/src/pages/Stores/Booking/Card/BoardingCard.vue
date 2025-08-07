9d21e9d3b6b9d580d3e8ff33257f04238a9e5230
TingShen <tingshen0609@gmail.com>
2025-08-07 17:35:44 +0800

    feat: 新增寵物預約卡片顯示功能

---
+++ frontend/src/pages/Stores/Booking/Card/BoardingCard.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+    boarding: Object
+});
+</script>
+
+<template>
+     <td>
+      <FontAwesomeIcon v-if="boarding.note && boarding.note.trim() !== ''" icon="fa-solid fa-bookmark" />
+    </td>
+    <td>{{ boarding.id }}</td>
+    <td>{{ boarding.customer_name }}</td>
+    <td>{{ boarding.phone }}</td>
+    <td>{{ boarding.pet_name }}</td>
+    <td>{{ boarding.pet_type }}</td>
+    <td>{{ boarding.booking_date }}</td>
+    <td>{{ boarding.room_type }}</td>
+    <td>{{ boarding.status }}</td>
+
+    <td v-if="boarding.status === '待審核'">
+        <RouterLink :to="`/stores/boarding-booking/review?id=${boarding.id}`">
+            <button class="btn">審核</button>
+        </RouterLink>
+        <button class="btn">取消</button>
+    </td>
+    <td v-else-if="boarding.status === '已審核'">
+        <RouterLink :to="`/stores/boarding-booking/review?id=${boarding.id}`">
+            <button class="btn">詳情</button>
+        </RouterLink>
+        <button class="btn">取消</button>
+        <button class="btn">已完成</button>
+    </td>
+    <td v-else>
+        <RouterLink :to="`/stores/boarding-booking/details?id=${boarding.id}`">
+            <button class="btn">訂單詳情</button>
+        </RouterLink>
+        <button class="btn">備註</button>
+        <button class="btn">列入觀察</button>
+    </td>
+</template>
+
+<style></style>
