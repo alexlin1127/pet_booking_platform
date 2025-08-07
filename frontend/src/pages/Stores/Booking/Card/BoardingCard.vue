b028e3715d9b8ae63e12a8afa76fe2cddbf3ed31
TingShen <tingshen0609@gmail.com>
2025-08-07 18:21:58 +0800

    style: 更新寵物預約及美容預約卡片按鈕樣式

--- frontend/src/pages/Stores/Booking/Card/BoardingCard.vue
+++ frontend/src/pages/Stores/Booking/Card/BoardingCard.vue
@@ -21,23 +21,23 @@ defineProps({
 
     <td v-if="boarding.status === '待審核'">
         <RouterLink :to="`/stores/boarding-booking/review?id=${boarding.id}`">
-            <button class="btn">審核</button>
+            <button class="boarding-btn">審核</button>
         </RouterLink>
-        <button class="btn">取消</button>
+        <button class="boarding-btn">取消</button>
     </td>
     <td v-else-if="boarding.status === '已審核'">
         <RouterLink :to="`/stores/boarding-booking/review?id=${boarding.id}`">
-            <button class="btn">詳情</button>
+            <button class="boarding-btn">詳情</button>
         </RouterLink>
-        <button class="btn">取消</button>
-        <button class="btn">已完成</button>
+        <button class="boarding-btn">取消</button>
+        <button class="boarding-btn">已完成</button>
     </td>
     <td v-else>
         <RouterLink :to="`/stores/boarding-booking/details?id=${boarding.id}`">
-            <button class="btn">訂單詳情</button>
+            <button class="boarding-btn">訂單詳情</button>
         </RouterLink>
-        <button class="btn">備註</button>
-        <button class="btn">列入觀察</button>
+        <button class="boarding-btn">備註</button>
+        <button class="boarding-btn">列入觀察</button>
     </td>
 </template>
 
