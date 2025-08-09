31c20556678011af3cc1c8f51ef59a1062b87cbd
TingShen <tingshen0609@gmail.com>
2025-08-09 16:04:06 +0800

    feat: 新增預約歷史紀錄卡片組件

---
+++ frontend/src/pages/Stores/Booking/Card/HistoryCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+    history: Object
+});
+</script>
+
+<template>
+    <td>{{ history.id }}</td>
+    <td>{{ history.customer_name }}</td>
+    <td>{{ history.phone }}</td>
+    <td>{{ history.pet_name }}</td>
+    <td>{{ history.pet_type }}</td>
+    <td>{{ history.booking_date }}</td>
+    <td>{{ history.service_type }}</td>
+    <td>{{ history.status }}</td>
+    <td v-if="history.status === '已完成'">
+        <RouterLink :to="`/stores/bookingshistory/details?id=${history.id}`">
+            <button class="history-btn">訂單詳情</button>
+        </RouterLink>
+        <button class="history-btn">備註</button>
+        <button class="history-btn">列入觀察</button>
+    </td>
+</template>
+
+<style></style>
