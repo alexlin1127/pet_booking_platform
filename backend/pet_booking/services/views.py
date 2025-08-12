5193294530215ee5bccfdffb9cacd4ab49f28c60
Joan <joanshen005@gmail.com>
2025-08-12 13:28:31 +0800

    refactor(serializers): 更新住宿與美容服務序列化邏輯，移除不必要的模型與欄位

--- backend/pet_booking/services/views.py
+++ backend/pet_booking/services/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from .models import BoardingService, GroomingService
+from pet_booking.stores.models import Store
 from .serializers import BoardingServiceSerializer, GroomingServiceSerializer
 from rest_framework.permissions import IsAuthenticated
 import django_filters
@@ -15,6 +16,7 @@ class ServicePagination(PageNumberPagination):
 
 
 class BoardingServiceViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     serializer_class = BoardingServiceSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = ServicePagination
@@ -22,25 +24,42 @@ class BoardingServiceViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return BoardingService.objects.filter(store_id__user_id=self.request.user)
 
+    def perform_create(self, serializer):
+        store = Store.objects.get(user_id=self.request.user)
+        serializer.save(store_id=store)
+
+
+
+    
+
+
 
 class GroomingServiceViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     serializer_class = GroomingServiceSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = ServicePagination
 
     def get_queryset(self):
         return GroomingService.objects.filter(store_id__user_id=self.request.user)
+    
+    def perform_create(self, serializer):
+        store = Store.objects.get(user_id=self.request.user)
+        serializer.save(store_id=store)
+
 
 
 # 使用者-店家服務
 class CustomerBoardingViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
+    permission_classes = []
     serializer_class = BoardingServiceSerializer
     queryset = BoardingService.objects.all()
     pagination_class = ServicePagination
 
 class CustomerGroomingViewset(viewsets.ModelViewSet):
     http_method_names = ['get']
+    permission_classes = []
     serializer_class = GroomingServiceSerializer
     queryset = GroomingService.objects.all()
     pagination_class = ServicePagination
