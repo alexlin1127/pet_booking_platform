0d292c6f4bfd368b6934f719e4d532344d9c7611
elmo <potato95101@gmail.com>
2025-08-13 01:39:15 +0000

    feat: 新增註冊驗證 API，包含發送驗證碼與確認驗證碼的功能，並更新相關序列化器與路由

--- backend/pet_booking/customers/models.py
+++ backend/pet_booking/customers/models.py
@@ -16,8 +16,8 @@ class CustomersProfile(models.Model):
     user_id = models.ForeignKey('users.User', to_field='user_id', on_delete=models.CASCADE, db_index=True, related_name='customersProfiles')
     full_name = models.CharField(max_length=255, null=False, blank=False)
     gender = models.CharField(max_length=6, choices=Gender.choices, null=True, blank=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
-    email = models.EmailField(null=False, blank=False)
+    phone = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
     address = models.CharField(max_length=512, null=True, blank=True)
 
     class Meta:
