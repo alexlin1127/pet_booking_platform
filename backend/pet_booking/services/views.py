2d125d556da8fcfb12a033de1a685470ba9ca73b
Alex <Like2968@gmail.com>
2025-08-16 00:45:25 +0800

    chore: 移除服務分頁設定的註解

--- backend/pet_booking/services/views.py
+++ backend/pet_booking/services/views.py
@@ -9,17 +9,17 @@ from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 #店家服務
-class ServicePagination(PageNumberPagination):
-    page_size = 2
-    page_size_query_param = 'page_size'
-    max_page_size = 10
+# class ServicePagination(PageNumberPagination):
+#     page_size = 2
+#     page_size_query_param = 'page_size'
+#     max_page_size = 10
 
 
 class BoardingServiceViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     serializer_class = BoardingServiceSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = ServicePagination
+    # pagination_class = ServicePagination
 
     def get_queryset(self):
         return BoardingService.objects.filter(store_id__user_id=self.request.user)
@@ -33,7 +33,7 @@ class GroomingServiceViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     serializer_class = GroomingServiceSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = ServicePagination
+    # pagination_class = ServicePagination
 
     def get_queryset(self):
         return GroomingService.objects.filter(store_id__user_id=self.request.user)
@@ -50,11 +50,11 @@ class CustomerBoardingViewSet(viewsets.ModelViewSet):
     permission_classes = []
     serializer_class = BoardingServiceSerializer
     queryset = BoardingService.objects.all()
-    pagination_class = ServicePagination
+    # pagination_class = ServicePagination
 
 class CustomerGroomingViewset(viewsets.ModelViewSet):
     http_method_names = ['get']
     permission_classes = []
     serializer_class = GroomingServiceSerializer
     queryset = GroomingService.objects.all()
-    pagination_class = ServicePagination
+    # pagination_class = ServicePagination
